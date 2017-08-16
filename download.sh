a6e9520684ec2ad95a8059eec3c4e54a6b09c2db
Zak Weiland <zweiland@gmail.com>
2017-08-16 13:12:28 -0500

    uh

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 # Pull the latest changes from the repo
 git pull
 
-files="web-ui-kit-marcosilva web-ui-kit-marcosilva2" # add a new file like this: "file1 file2 file3"
+files="design-system-hostgator" # add a new file like this: "file1 file2 file3"
 for f in $files
 do
   # Zip core Sketch data
